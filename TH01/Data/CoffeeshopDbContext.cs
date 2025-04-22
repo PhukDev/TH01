754b927cbd2777cef9a0a92daf962099ac38dafd
ITD <ksphuk2005@gmail.com>
2025-04-22 10:49:30 +0700

    section 2

---
+++ TH01/Data/CoffeeshopDbContext.cs
@@ -0,0 +1,14 @@
+﻿using TH01.Models;
+using Microsoft.EntityFrameworkCore;
+namespace TH01.Data
+
+{
+    public class CoffeeshopDbContext : DbContext
+    {
+        public CoffeeshopDbContext(DbContextOptions<CoffeeshopDbContext> options) :
+base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
